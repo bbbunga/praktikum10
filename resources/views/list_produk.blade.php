3683c945c687cf74769783cec9dec7a8be8bd0dd
bbbunga <bungasitumorang738@gmail.com>
2025-06-04 14:32:36 +0700

    First commit

--- resources/views/list_produk.blade.php
+++ resources/views/list_produk.blade.php
@@ -83,7 +83,6 @@
     </div>
 </div>
 
-<!-- Modal Edit -->
 <div id="editModal" class="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center hidden z-50">
     <div class="bg-white p-6 rounded shadow max-w-lg w-full relative">
         <button onclick="closeModal()" class="absolute top-2 right-2 text-gray-600 hover:text-black">✕</button>
